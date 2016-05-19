f902ee9fb59340358bc673ea9abb4316225096ee
OlegUshakov <derby@nm.ru>
2016-05-19 22:42:45 +0300

    casesensitive filename

---
+++ app/src/main/java/com/example/officepcdell/githubusersearch/classes/SearchRequest.java
@@ -0,0 +1,10 @@
+package com.example.officepcdell.githubusersearch.classes;
+
+public class SearchRequest {
+    public final String queryText;
+
+    /** Constructor */
+    public SearchRequest(String queryText){
+        this.queryText = queryText;
+    }
+}
